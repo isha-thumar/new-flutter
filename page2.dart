7ad7c57b3204328d993d7c5b45c322c01f1e2d49
isha-thumar <109851136+isha-thumar@users.noreply.github.com>
2023-01-23 23:03:05 +0530

    Update page2.dart

--- page2.dart
+++ page2.dart
@@ -14,15 +14,22 @@ class _allproState extends State<allpro> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:AppBar(title: Text(widget.data!['title']),),
-      body: Column(
+      body:Container(height: 1000,decoration: BoxDecoration(gradient: LinearGradient(colors: [
+
+       Colors.deepPurple,
+        Colors.white
+        ],)),
+        child:Column(
         children: [Padding(padding: EdgeInsets.all(20)),
           Center(child: Text(widget.data!['des'],style: TextStyle(fontSize: 40,color: Colors.white,backgroundColor:Colors.black,  ),),
           ),
           SizedBox(height: 20,),
           Container(decoration:BoxDecoration(border: Border.all(color: Colors.black),shape: BoxShape.rectangle),child:Image(image: NetworkImage(widget.data!['images']))),
+        SizedBox(height:10),
         Center(child: Container(child: Center(child: Text(widget.data!['info'],style: TextStyle(fontSize: 30),)),))
         ],
       ),
-    );
+      ),);
   }
+}
 
