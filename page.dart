97c236cca79a2eaa6e71c041db82b30897065c54
isha-thumar <109851136+isha-thumar@users.noreply.github.com>
2023-01-23 23:02:31 +0530

    Update page.dart

--- page.dart
+++ page.dart
@@ -12,19 +12,19 @@ class _appState extends State<app> {
   List<Map> data=[{
     'title':'Pizza',
     'images':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjCZ2SNHdWsZVUSigJTsjAchnlWBpnt_KMFA&usqp=CAU',
-    'des':'its pizza ',
+    'des':'pizza ',
      'info':'• Pizza    \n Onion Pizza 100\n Panner Pizza  165     \n Mashroom Pizza  180    \nPepperoni Pizza 200    '
 
   },
     {
       'title':'Burger',
       'images':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzqE1WhGdPm0YU2eL7z65ktGZVKfKBw_6-2w&usqp=CAU',
-      'des':'its Burger',
+      'des':'Burger',
     'info':'• Burger \n Veg.Burger 100 \nVeg Chili Cheese 250  \n Panner King 199\n Xtra long Maxican Veg 100'
     },{
       'title':'Cake',
       'images':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-xbbtOjenth-o4vYlvVB_CUXp40wW3CF2pg&usqp=CAU',
-      'des':'its cake',
+      'des':' cake',
       'info':'• Cake \nChoco Delight 400\n Black Forset 400\n Choco Chip 400 \n Choco Rice 400 \n Choco Walnut 800'
 
     },
@@ -38,28 +38,28 @@ class _appState extends State<app> {
     {
       'title':'Sandwich',
       'images':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMO35KBZy7FkH4IHUoV8KvsbrjKUGSVL5aZQ&usqp=CAU',
-      'des':'its sandwich',
+      'des':'sandwich',
     'info':'• Sandwich \nVeg Sandwich 90\n New Chilli Cheese Grilled 99\n Potato Cheese Sandwich 100\n Mayo Cheese Sandwich 100'
 
     },
     {
       'title':'Panipuri',
       'images':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTupcdVYTqa0CuntmNQEx0J3eIPzotq0zT8Jw&usqp=CAU',
-      'des':'its panipuri',
+      'des':' panipuri',
       'info':'• Panipuri\n Sev Puri 50\n Dahi Puri 60\n Ragda puri 40\n Masala dahi Puri 35'
 
     },
     {
       'title':'Manchurian',
       'images':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAQbGAgEU8w3kPuUL0a6ZxSczP1QtQOdPHHw&usqp=CAU',
-      'des':'its manchurian',
+      'des':'manchurian',
       'info':'•Manchurian\n Gravy Manchurian 140\n DRY Manchurian 140\n Hakkka Noodles 70'
 
     },
     {
       'title':'Pasta',
       'images':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-vYNRZ5LeT7ay8y25z1gd_zfw7VWoKjiVuQ&usqp=CAU',
-      'des':'its pasta'
+      'des':' pasta'
       ,'info':'• Pasta \n Masala Pasta 70\n Veg.Masala Pasta 80\n Tomato Pasta 80\n Butter masala Pasta 80\n Cheeze masala Pasta 90\n Veg.Cheese Pasta 90'
 
     },
@@ -85,7 +85,11 @@ class _appState extends State<app> {
     return Scaffold(
 
         appBar: AppBar(title: Text('Menu',style: TextStyle(color: Colors.black,fontSize: 40),),),
-        body: GridView.builder(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        body:Container(height: 1000,decoration: BoxDecoration(gradient: LinearGradient(colors: [
+          Colors.deepPurple,
+          Colors.white
+             ],)),
+        child:GridView.builder(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
           itemCount: data.length,itemBuilder:(context, index) => Padding(padding: EdgeInsets.all(18.0),
             child: GestureDetector(onTap: (){
               Navigator.push(context, MaterialPageRoute(builder: (context) => allpro(data[index]),));
@@ -97,6 +101,6 @@ class _appState extends State<app> {
                 alignment: Alignment.bottomCenter,
                 child: Text(data[index]['title'],style: TextStyle(color: Colors.white,fontSize: 20,backgroundColor: Colors.black),),
               ),),), )
-    );
+        ), );
   }
 }
