161fd66c4245221d7d93df97ffbe6dd80ff8c18d
isha-thumar <109851136+isha-thumar@users.noreply.github.com>
2023-01-24 09:15:09 +0530

    Update page.dart

--- page.dart
+++ page.dart
@@ -13,14 +13,14 @@ class _appState extends State<app> {
     'title':'Pizza',
     'images':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjCZ2SNHdWsZVUSigJTsjAchnlWBpnt_KMFA&usqp=CAU',
     'des':'pizza ',
-     'info':'• Pizza    \n Onion Pizza 100\n Panner Pizza  165     \n Mashroom Pizza  180    \nPepperoni Pizza 200    '
+    'info':'• Pizza    \n Onion Pizza 100\n Panner Pizza  165     \n Mashroom Pizza  180    \nPepperoni Pizza 200    '
 
   },
     {
       'title':'Burger',
       'images':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzqE1WhGdPm0YU2eL7z65ktGZVKfKBw_6-2w&usqp=CAU',
       'des':'Burger',
-    'info':'• Burger \n Veg.Burger 100 \nVeg Chili Cheese 250  \n Panner King 199\n Xtra long Maxican Veg 100'
+      'info':'• Burger \n Veg.Burger 100 \nVeg Chili Cheese 250  \n Panner King 199\n Xtra long Maxican Veg 100'
     },{
       'title':'Cake',
       'images':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-xbbtOjenth-o4vYlvVB_CUXp40wW3CF2pg&usqp=CAU',
@@ -39,7 +39,7 @@ class _appState extends State<app> {
       'title':'Sandwich',
       'images':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMO35KBZy7FkH4IHUoV8KvsbrjKUGSVL5aZQ&usqp=CAU',
       'des':'sandwich',
-    'info':'• Sandwich \nVeg Sandwich 90\n New Chilli Cheese Grilled 99\n Potato Cheese Sandwich 100\n Mayo Cheese Sandwich 100'
+      'info':'• Sandwich \nVeg Sandwich 90\n New Chilli Cheese Grilled 99\n Potato Cheese Sandwich 100\n Mayo Cheese Sandwich 100'
 
     },
     {
@@ -67,7 +67,7 @@ class _appState extends State<app> {
       'title':'Frankey',
       'images':'https://t3.ftcdn.net/jpg/05/46/85/74/240_F_546857433_qEYx5SJpteaMAgZs89Z3g7hGFktwJEFD.jpg',
       'des':'SUPER tasty',
-    'info':'•Frankey \n Veg.Frankey 60\n Schezwan paneer Frankey 80\nChesse frankey 50\nChilli paneer frankey 80'
+      'info':'•Frankey \n Veg.Frankey 60\n Schezwan paneer Frankey 80\nChesse frankey 50\nChilli paneer frankey 80'
 
     },
 
@@ -75,7 +75,7 @@ class _appState extends State<app> {
       'title':'Noodles',
       'images':'https://img.freepik.com/free-photo/schezwan-noodles-szechwan-vegetable-hakka-noodles-chow-mein-is-popular-indo-chinese-recipes-served-bowl-plate-with-wooden-chopsticks_466689-74642.jpg?size=626&ext=jpg&ga=GA1.2.71060362.1673925701&semt=sph',
       'des':'Noodles',
-    'info':'•Noodles\n Veggies Noodles 50\n Spicy Noodles 80\nChicken Noodles 90\n cold Noodles 100'
+      'info':'•Noodles\n Veggies Noodles 50\n Spicy Noodles 80\nChicken Noodles 90\n cold Noodles 100'
 
     },
 
@@ -83,24 +83,20 @@ class _appState extends State<app> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-        appBar: AppBar(title: Text('Menu',style: TextStyle(color: Colors.black,fontSize: 40),),),
-        body:Container(height: 1000,decoration: BoxDecoration(gradient: LinearGradient(colors: [
-          Colors.deepPurple,
-          Colors.white
-             ],)),
-        child:GridView.builder(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-          itemCount: data.length,itemBuilder:(context, index) => Padding(padding: EdgeInsets.all(18.0),
-            child: GestureDetector(onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => allpro(data[index]),));
-            },
-              child: Container(
-                decoration:BoxDecoration(border: Border.all(style: BorderStyle.solid,color: Colors.black),
-                    color:Colors.green,image: DecorationImage(fit: BoxFit.fill,image: NetworkImage(data[index]['images']))
-                ) ,
-                alignment: Alignment.bottomCenter,
-                child: Text(data[index]['title'],style: TextStyle(color: Colors.white,fontSize: 20,backgroundColor: Colors.black),),
-              ),),), )
-        ), );
+         backgroundColor: Colors.black54,
+      appBar: AppBar(title: Text('Menu',style: TextStyle(color: Colors.white,fontSize: 40),),
+          backgroundColor: Colors.black,),
+      body: GridView.builder(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            itemCount: data.length,itemBuilder:(context, index) => Padding(padding: EdgeInsets.all(18.0),
+              child: GestureDetector(onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => allpro(data[index]),));
+              },
+                child: Container(
+                  decoration:BoxDecoration(border: Border.all(style: BorderStyle.solid,color: Colors.black),borderRadius: BorderRadius.all(Radius.circular(15)),
+                      color:Colors.green,image: DecorationImage(fit: BoxFit.fill,image: NetworkImage(data[index]['images']))
+                  ) ,
+                  alignment: Alignment.bottomCenter,
+                  child: Text(data[index]['title'],style: TextStyle(color: Colors.white,fontSize: 20,backgroundColor: Colors.black),),
+                ),),), )
+       );
   }
-}
